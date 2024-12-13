49e1e08fe20263799e578ee881bb658b03d2e5ac
KirillYarv <yarovoykirill942@gmail.com>
2024-12-13 13:53:53 +0300

    ORM

--- local/php_interface/s1/init.php
+++ local/php_interface/s1/init.php
@@ -1,6 +1,11 @@
 <?php
 
-$arJsConfig = array( 
+spl_autoload_register(function ($class) {
+    include '/home/bitrix/www/local/modules/somepartner.mybookscatalog/lib/BookTable.php';
+});
+
+
+$arJsConfig = array(
 	'custom_main' => array( 
 		'js' => '/local/templates/exam1/js/mine.js', 
 		'css' => '',
@@ -10,4 +15,7 @@ $arJsConfig = array(
 foreach ($arJsConfig as $ext => $arExt) { 
 	\CJSCore::RegisterExt($ext, $arExt); 
 }
+require '/home/bitrix/www/local/modules/somepartner.mybookscatalog/lib/BookTable.php';
+#\SomePartner\MyBooksCatalog\BookRebirthTable::getEntity()->compileDbTableStructureDump();
+
 
